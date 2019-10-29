339c266d1a28478c714747a1b2ad05eb7a984cef
Grayce Ferreira <grayce.ferreira@gmail.com>
2019-10-29 13:29:52 -0300

    Atualizando

--- 00b_revisao/index.js
+++ 00b_revisao/index.js
@@ -16,16 +16,39 @@
 const body = document.body;
 const botao = document.getElementById('trocaCor');
 
-body.style.backgroundColor = 'purple';
+// body.style.backgroundColor = 'purple';
 
 
-botao.addEventListener('click', () => {
-    const r = geraInteriro(255);
-    const g = geraInteriro(255);
-    const b = geraInteriro(255);
-    body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')'
-});
+// botao.addEventListener('click', () => {
+//     const r = geraInteriro(255);
+//     const g = geraInteriro(255);
+//     const b = geraInteriro(255);
+//     body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')'
+//         // `rgb (${r},${g},${b})`;
+// });
 
 
-geraInteriro = num =>
-    parseInt(Math.random() * num);
+// geraInteriro = num =>
+//     parseInt(Math.random() * num);
+
+
+
+class MudaCor {
+    constructor() {
+        this.r = this.geraInteriro(255);
+        this.g = this.geraInteriro(255);
+        this.b = this.geraInteriro(255);
+    }
+    geraInteriro(num) {
+        return parseInt(Math.random() * num)
+    };
+    geraRGN() {
+        return `rgb(${this.r},${this.g},${this.b})`
+    };
+};
+
+function mudaBody() {
+    const mudaC = new MudaCor()
+    body.style.backgroundColor = mudaC.geraRGN();
+
+}
