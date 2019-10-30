76ed970f0e4fe5d8d41490a593856d597884096d
carvalhojenni <jenniferdsr@gmail.com>
2019-10-30 17:56:31 -0300

    Resolvendo exercicio

--- 01f_ajax_giphy_search_input/index.js
+++ 01f_ajax_giphy_search_input/index.js
@@ -1,24 +1,30 @@
 // XMLHttpRequest na rota search capturando informação do input
 // trazer 10 imagens
 // apresentar as imagens na div #root
-const quero = ""
-const url = 'http://api.giphy.com/v1/gifs/search?q=' + quero + '&api_key=dc6zaTOxFJmzC&limit=3&offset=0'
+
+const input1 = document.getElementById('gifInput')
+const url = 'http://api.giphy.com/v1/gifs/search?q=' + input1 + '&api_key=dc6zaTOxFJmzC&limit=3&offset=0'
+const div = document.getElementById('root')
 
 const request = new XMLHttpRequest()
-request.onreadystatechange = mostraGif;
+request.onreadystatechange = myFunction;
 request.open('GET', url);
 request.send();
 
-function mostraGif() {
+function myFunction() {
     if (request.readyState === 4 && request.status === 200) {
         const response = request.response;
         const json = JSON.parse(response);
-        console.log(json);
+        const data = json.data;
+        const imgUrl = json.data[0].images.original.url;
 
-        const imgUrl = json.data.images.original.url;
-
-        const img = document.getElementById('imagem');
-
-        // img.src = imgUrl;
+        if (input1.checkValidity()) {
+            document.getElementById("gifButton").innerHTML = input1.validationMessage;
+            div.innerHTML += `
+            <img src=${data[0].imagem} >
+            `
+        }
+    } else {
+        document.getElementById("gifButton").innerHTML = "Input OK";
     }
 }
