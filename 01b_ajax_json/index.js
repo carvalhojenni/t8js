a39d92e8b05071f90f963d0ef4e613572c0af013
Grayce Ferreira <grayce.ferreira@gmail.com>
2019-10-30 16:42:07 -0300

    Resolvendo exercicios

--- 01b_ajax_json/index.js
+++ 01b_ajax_json/index.js
@@ -8,3 +8,37 @@
 // [] iterar sobre o array e mostrar as três imagens usando for()
 // [] iterar sobre o array e mostrar as três imagens usando for...of
 // [] iterar sobre o array e mostrar as três imagens usando forEach
+const url = './data/dados.json'
+
+const request = new XMLHttpRequest()
+const div = document.getElementById('root');
+request.open('GET', url)
+request.send()
+
+request.onreadystatechange = mostrarImg;
+
+
+function mostrarImg() {
+    if (request.readyState === 4 && request.status === 200) {
+        const response = request.response;
+        const json = JSON.parse(response);
+        const data = json.data;
+        criarImagens(data)
+    }
+}
+
+function criarImagens(array) {
+    div.innerHTML = `
+            <img src = ${data[0].imagem}>
+            <img src = ${data[1].imagem}>
+            <img src = ${data[2].imagem}>
+            `
+}
+
+function criarImagensFor(array) {
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        const img = `<img src= ${element.imagem}>`;
+        div.innerHTML += img
+    }
+}
--- 01d_ajax_giphy_random/index.js
+++ 01d_ajax_giphy_random/index.js
@@ -5,3 +5,37 @@
 // [] transformar o dado recebido
 // [] apresentar <img>
 // [] usar async/await, then(), .catch(), arrow function
+
+const request = new XMLHttpRequest()
+
+const quero = "mulher maravilha"
+const url = 'http://api.giphy.com/v1/gifs/search?q=' + quero + '&api_key=dc6zaTOxFJmzC&limit=3&offset=0'
+
+request.onreadystatechange = mostraGif;
+request.open('GET', url);
+request.send();
+
+function mostraGif() {
+    if (request.readyState === 4 && request.status === 200) {
+        const response = request.response;
+        const json = JSON.parse(response);
+        // const data1 = (json.data[0].images.original.url)
+        // const data2 = (json.data[1].images.original.url)
+        // const data3 = (json.data[2].images.original.url)
+        // const body = document.body
+        // const img1 = document.getElementById('imagem').src = data1
+        // const img2 = document.createElement('img')
+        // const img3 = document.createElement('img')
+        // body.appendChild(img2).src = data2
+        // body.appendChild(img3).src = data3
+        // document.querySelector('#imagem').src = imgSrc
+        const body = document.body;
+        body.innerHTML = `
+        <img src = ${json.data[0].images.original.url}>
+        <img src = ${json.data[1].images.original.url}>
+        <img src = ${json.data[2].images.original.url}>
+        `
+        console.log(json);
+
+    }
+}
